84c1170ec8ce34680e15f06d7b0f17694d6e85d9
bd-jhunter <sjhunter.sc@outlook.com>
2019-06-02 18:19:33 +0800

    update basic animation ppt

---
+++ Animation/MattBall/MattBall/Ball/MetaBallView.swift
@@ -0,0 +1,21 @@
+//
+//  MetaBallView.swift
+//  MattBall
+//
+//  Created by JHunter on 2019/6/1.
+//  Copyright © 2019 com.jhunter. All rights reserved.
+//
+
+import UIKit
+
+class MetaBallView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Animation/MattBall/MattBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MattBall
+//
+//  Created by JHunter on 2019/6/1.
+//  Copyright © 2019 com.jhunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
